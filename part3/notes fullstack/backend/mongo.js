d23f726ed3a91926852f063952ad4c65cc55b5ab
Roxc2301 <131661824+Roxc2301@users.noreply.github.com>
2023-09-19 16:27:38 +0200

    MongoDB and Atlas con Notes

--- part3/notes fullstack/backend/mongo.js
+++ part3/notes fullstack/backend/mongo.js
@@ -8,7 +8,7 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://jmoisesn23:${password}@cluster0.fh9x2un.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://jmoisesn23:${password}@cluster0.fh9x2un.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
