f3581072a16d6a1eca5957b2f819d675ddd46ea5
Roxc2301 <131661824+Roxc2301@users.noreply.github.com>
2023-09-20 16:26:27 +0200

    Exercises 3.13.-3.14.

--- part3/Notes/backend/index.js
+++ part3/Notes/backend/index.js
@@ -23,8 +23,13 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+  Note.findById(request.params.id)
+  .then(note => {
+    if(note){
+      response.json(note)
+    }else{
+      response.status(404).end()
+    }
   })
 })
 
--- part3/Phonebook/phonebook-backend/index.js
+++ part3/Phonebook/phonebook-backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 
+
+const Person = require('./models/person')
 
 const app = express();
 
@@ -8,74 +11,66 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-let persons = [
-  {
-    "name": "Jhonatan Nuñez",
-    "number": "9999",
-    "id": 1
-  },
-  {
-    "name": "Moises Zuniga",
-    "number": "9999",
-    "id": 2
-  }, {
-    "name": "Pablito",
-    "number": "12345",
-    "id": 3
-  }
-]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello Phonebook</h1>')
-})
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
+
+
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  
+  Person.findById(request.params.id).then(p => (response.json(p)))
 })
 
 
 app.delete('/api/persons/:id', (request, response) =>{
+  Person.findByIdAndDelete(request.params.id)
+  .then((deletedPerson) => response.json(deletedPerson))
+})
+
+app.put('/api/persons/:id', (request, response) => {
+  const updatedData = request.body; 
+
+  Person.findByIdAndUpdate(request.params.id, updatedData, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    });
+});
+
+
+
+app.post('/api/persons', (request, response) =>{
+  const body = request.body
+
+  if(body.name === undefined) return response.status(400).json({error: 'name is missing'})
+  if(body.number === undefined) return response.status(400).json({error: 'number is missing'})
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson =>{
+    response.json(savedPerson)
+  })
+})
+
+/* 
+app.delete('/api/persons/:id', (request, response) =>{
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
   response.status(204).end()
-})
-
+}) 
 const generateId = () => {
   const maxId = persons.length > 0
     ? Math.max(...persons.map(person => person.id))
     : 0
   return maxId + 1
 }
-
-app.post('/api/persons', (request, response) =>{
-  const body = request.body
-  if(!body.name || !body.number){
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: generateId(),
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
-
-})
+ */
 
 
 const PORT = process.env.PORT || 3001
