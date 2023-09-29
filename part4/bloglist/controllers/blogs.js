755329ba6ebfca81c19b0adf526b619ba4e0f017
Roxc2301 <131661824+Roxc2301@users.noreply.github.com>
2023-09-29 11:50:47 +0200

    4.13-4.14 testing is pending.

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -7,6 +7,30 @@ blogsRouter.get('/', (request, response) => {
     })
 })
 
+blogsRouter.get('/:id', (request, response, next) => {
+
+    Blog.findById(request.params.id)
+        .then(blog => {
+            if (blog) {
+                response.json(blog)
+            }
+            else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+blogsRouter.delete('/:id', (request, response, next) =>{
+
+    Blog.findByIdAndDelete(request.params.id)
+    .then((deletedBlog) =>{
+        response.json(deletedBlog)
+    })
+    .catch(error => next(error))
+
+})
+
 blogsRouter.post('/', (request, response, next) => {
     const body = request.body
 
@@ -24,5 +48,25 @@ blogsRouter.post('/', (request, response, next) => {
         })
         .catch(error => next(error))
 })
+
+blogsRouter.put('/:id', (request, response, next) =>{
+
+    const updatedData = request.body
+
+    Blog.findByIdAndUpdate(
+        request.params.id, updatedData, { new: true, runValidators: true, context: 'query'})
+        .then((updatedBlog) => {
+            if(updatedBlog){
+                response.json(updatedBlog)
+            }
+            else{
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+
+})
+
+
 
 module.exports = blogsRouter
